bab8de0eed1e572deba4b9e02953b868f884682b
ffomorfaruk333-blip <ffomorfaruk333@gmail.com>
2026-08-03 13:55:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const btn = document.querySelector(".btn");
 
   if (btn) {
-    btn.addEventListener("click", function (e) {
-      e.preventDefault();
-      alert("🎮 Welcome to OMOR 2.0 Gaming!");
+    btn.addEventListener("click", function () {
+      console.log("🎮 Welcome to OMOR 2.0 Gaming!");
     });
   }
 });
